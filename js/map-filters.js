49e1ab12c94e1fa27c66b3f8b3cec6dec1f94742
Ivan Davydov <59295002+ivan-davy@users.noreply.github.com>
2022-07-10 16:51:00 +0300

    Update map-filters.js

--- js/map-filters.js
+++ js/map-filters.js
@@ -47,7 +47,7 @@ const renderMainPin = () => {
     {
       draggable: true,
       icon: L.icon({
-        iconUrl: '/img/main-pin.svg',
+        iconUrl: './img/main-pin.svg',
         iconSize: [52, 52],
         iconAnchor: [26, 52]
       })
@@ -81,7 +81,7 @@ const renderMap = (properties) => {
       location,
       {
         icon: L.icon({
-          iconUrl: '/img/pin.svg',
+          iconUrl: './img/pin.svg',
           iconSize: [40, 40],
           iconAnchor: [20, 40]
         })
